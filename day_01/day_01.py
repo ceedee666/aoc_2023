411a6098bb6c75fb764ae0297a02fc6839cf9c4c
Christian Drumm <christian.drumm@hey.com>
2024-01-28 16:39:32 +0100

    improved version of day 1 solution.

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -5,61 +5,53 @@ import typer
 
 app = typer.Typer()
 
+DIGIT_MAP = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
 
-def read_input_file(input_file_path: str):
+
+DIGIT_REGEX = re.compile(r"(?=(\d|" + "|".join(DIGIT_MAP.keys()) + "))")
+
+
+def read_input_file(input_file_path: str) -> list:
     p = Path(input_file_path)
 
     with p.open() as f:
         lines = f.readlines()
 
-    return list(map(lambda line: line.strip(), lines))
+    return [line.strip() for line in lines]
 
 
-def solve_part_1(data: list[str]):
-    digits = [list(filter(lambda str: str.isdigit(), line)) for line in data]
-    calibration_values = [int(line[0] + line[-1]) for line in digits]
+def solve(data: list[str]) -> int:
+    converted_lines = [[int(c) for c in line if c.isdigit()] for line in data]
+    calibration_values = [line[0] + line[-1] for line in converted_lines]
     return sum(calibration_values)
 
 
 def map_digits(line: str):
-    digit_map = {
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-    }
-
-    mapped_line = ""
-    regex = r"(?=(\d|" + "|".join(digit_map.keys()) + "))"
-    for m in re.findall(regex, line):
-        if m.isdigit():
-            mapped_line += m
-        else:
-            mapped_line += digit_map[m]
-
+    mapped_line = "".join(DIGIT_MAP.get(d, d) for d in DIGIT_REGEX.findall(line))
     return mapped_line
-
-
-def solve_part_2(data: list[str]):
-    data = [map_digits(line) for line in data]
-    return solve_part_1(data)
 
 
 @app.command()
 def part_1(input_file: str):
     data = read_input_file(input_file)
-    print(f"The sum of all calibration values is {solve_part_1(data)}")
+    print(f"The sum of all calibration values is {solve(data)}")
 
 
 @app.command()
 def part_2(input_file: str):
     data = read_input_file(input_file)
-    print(f"The sum of all calibration values is {solve_part_2(data)}")
+    data = [map_digits(line) for line in data]
+    print(f"The sum of all values is {solve(data)}")
 
 
 if __name__ == "__main__":
